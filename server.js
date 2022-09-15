d609368c633b4f3f2df0c7d1230f7dd142f0a183
Ahad Ahmadi <ab.ahmadiofficial@gmail.com>
2022-09-16 00:26:56 +0430

    added friends route

--- server.js
+++ server.js
@@ -4,15 +4,37 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello')
+const friends = [
+    {
+        id: 0,
+        name: 'John',
+    },
+    {
+        id: 1,
+        name: 'Jane',
+    }
+
+];
+
+app.get('/friends', (req, res) => {
+    res.json(friends);
+
 })
 
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if(friend) {
+        res.status(200).json(friend);
+    }else {
+        res.status(404).json({message: 'Not found'});
+        error: 'Friends not found'
+    }
+})
+
+
 app.get('/messages', (req, res) => {
-    res.send({
-        id: 1,
-        message: 'Hello World!'
-    })
+    res.send('<ul><li>Hello Ahad!</li></ul>')
 })
 
 app.post('/messages', (req, res) => {
