e69244af240f80c4d630f9c39155c42ffad7f226
Ahad Ahmadi <ab.ahmadiofficial@gmail.com>
2022-09-16 13:06:06 +0430

    added node mon package

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 const friends = [
     {
         id: 0,
-        name: 'John',
+        name: 'ahad',
     },
     {
         id: 1,
@@ -15,6 +15,15 @@ const friends = [
     }
 
 ];
+
+app.use((req, res, next) => {
+    const start = new Date().now();
+    
+    next();
+    // actions go here...
+    const delta = Date().now() - start;
+    console.log(`${req.method} request for '${req.url}`);
+});
 
 app.get('/friends', (req, res) => {
     res.json(friends);
